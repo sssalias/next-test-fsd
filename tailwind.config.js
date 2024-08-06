e15838b6096159f8ce71cbf2b401e1a8e79a63e9
moverq1337 <moverq1337@gmail.com>
2024-08-07 00:19:55 +0300

    mama mia2

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,42 @@ module.exports = {
 				'4xl': ['40px', '45px'], // H2
 				'5xl': ['64px', '64px'], // H1 + banner 1
 			},
+			colors: {
+				transparent: 'transparent', // Прозрачный
+				current: 'currentColor', // Текущий цвет
+				white: '#ffffff', // Белый
+				purple: '#3f3cbb', // Фиолетовый
+				midnight: '#121063', // Полуночный
+				metal: '#565584', // Металлический
+				tahiti: '#3ab7bf', // Таити
+				silver: '#ecebff', // Серебряный
+				'bubble-gum': '#ff77e9', // Баблгам
+				bermuda: '#78dcca', // Бермуда
+				'base-white': '#FFFFFF', // Белый базовый
+				'base-light-grey': '#FAFAFA', // Светло-серый базовый
+				'base-grey': '#F4F4F5', // Серый базовый
+				'base-dark-grey': '#E4E4E7', // Тёмно-серый базовый
+				'base-light-silver': '#D4D4D8', // Светло-серебристый базовый
+				'base-silver': '#777675', // Серебристый базовый
+				'base-dark-silver': '#52525B', // Тёмно-серебристый базовый
+				'base-charcoal': '#3F3F46', // Угольный базовый
+				'base-dark-charcoal': '#27272A', // Тёмно-угольный базовый
+				'base-black': '#18181B', // Чёрный базовый
+				'primary-blue': '#006FEE', // Синий основной
+				'primary-light-blue': '#21A5EE', // Светло-синий основной
+				'primary-red': '#EF5050', // Красный основной
+				'primary-green': '#00C974', // Зелёный основной
+				'primary-yellow': '#FAC100', // Жёлтый основной
+				'primary-grey': '#A6A6B0', // Серый основной
+				'secondary-light-blue': '#E9F6FF', // Светло-голубой вспомогательный
+				'secondary-light-purple': '#DDD0F9', // Светло-фиолетовый вспомогательный
+				'secondary-pink': '#F9D0D0', // Розовый вспомогательный
+				'secondary-light-green': '#D0F9D9', // Светло-зелёный вспомогательный
+				'secondary-lavender': '#BDCEF9', // Лавандовый вспомогательный
+				'secondary-light-yellow': '#FFF3CA', // Светло-жёлтый вспомогательный
+				'gradient-blue': '#21A5EE - #006FEE', // Градиент от светло-синего к синему
+				'gradient-red': '#EF5050 - #F12727', // Градиент от красного к тёмно-красному
+			},
 		},
 		darkMode: 'class',
 		plugins: [nextui()],
